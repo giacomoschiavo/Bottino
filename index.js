d9aa6ac709a265e20594c262f40518a1b666f995
giacomoschiavo <giacomo.schiavo12@gmail.com>
2020-11-01 15:51:15 +0100

    pong

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ client.login(process.env.BOT_TOKEN);
 
 client.on('message', message => {
    if(message.content === `${prefix}ping`) {
-      message.channel.send("peng!");
+      message.channel.send("pong!");
    } 
    if(message.content.startsWith(`${prefix}meaning of life`)) {
       message.channel.send(config.meaning_of_life);
