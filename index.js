af40c33b7d80a9f29dc0717b2c7eca0399105be7
Giacomo Schiavo <42863424+giacomoschiavo@users.noreply.github.com>
2020-11-01 14:17:58 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const {prefix, token} = require("./config.json");
+const client = new Discord.Client();
+
+client.once('ready', () => {
+   console.log('Ready!');
+});
+
+client.login(process.env.BOT_TOKEN);
+
+client.on('message', message => {
+   if(message.content === `${prefix}ping`) {
+      message.channel.send("pong!");
+   } 
+   if(message.content.startsWith(`${prefix}meaning of life`)) {
+      message.channel.send(config.meaning_of_life);
+   }
+});
+
