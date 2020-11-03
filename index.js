e22cbaf7469d4fd75da50122cc2862dac3440e06
giacomoschiavo <giacomo.schiavo12@gmail.com>
2020-11-03 08:18:35 +0100

    added some useless test functions

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const {prefix, token} = require("./config.json");
+const {prefix} = require("./config.json");
 const client = new Discord.Client();
 
 client.once('ready', () => {
@@ -8,12 +8,68 @@ client.once('ready', () => {
 
 client.login(process.env.BOT_TOKEN);
 
-client.on('message', message => {
-   if(message.content === `${prefix}ping`) {
-      message.channel.send("pong!");
-   } 
-   if(message.content.startsWith(`${prefix}meaning of life`)) {
-      message.channel.send(config.meaning_of_life);
-   }
+client.on('message', msg => {
+   if(!msg.content.startsWith(prefix) || msg.author.bot) return;
+   
+   const args = msg.content.slice(prefix.length).trim().split(/ +/);
+   const command = args.shift().toLowerCase();
+
+   if(command === "args-info") {
+      msg.channel.send(`Il comando usato: ${command}\nArgomenti: ${args}`);
+   }   
 });
 
+
+// client.on('message', msg => {
+//    if(!msg.content.startsWith(prefix) || msg.author.bot) return;
+   
+//    const args = msg.content.slice(prefix.length).trim().split(/ +/);
+//    const command = args.shift().toLowerCase();
+
+//    if(command === "kick") {
+//       if(!msg.mentions.users.size) 
+//          return msg.reply("Hai bisogno di menzionare qualcuno :/")
+//       const taggerUser = msg.mentions.users.first();
+//       msg.channel.send(`You wanted to kick: ${taggerUser.username}`);
+//    }
+
+// });
+
+// client.on('message', msg => {
+//    if(!msg.content.startsWith(prefix) || msg.author.bot) return;
+   
+//    const args = msg.content.slice(prefix.length).trim().split(/ +/);
+//    const command = args.shift().toLowerCase();
+
+//    if(command === "avatar") {
+//       if(!msg.mentions.users.size){ 
+//          return msg.channel.send(`Il tuo avatar: <${msg.author.displayAvatarURL({format: "png", dynamic: true})}>`);
+//       }
+
+//       const avatarList = msg.mentions.users.map(user => `${user.username}'s avatar: <${user.displayAvatarURL({format: "png", dynamic: true})}>`);
+//       return msg.channel.send(avatarList);
+//    }
+
+// });
+
+client.on("message", message => {
+   if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+   const args = message.content.slice(prefix.length).trim().split(/ +/);
+   const command = args.shift().toLowerCase();
+   if(command === "prune") {
+      const amount = parseInt(args[0]) + 1;
+
+		if (isNaN(amount)) {
+			return message.reply('that doesn\'t seem to be a valid number.');
+		} else if (amount <= 1 || amount > 100) {
+			return message.reply('you need to input a number between 1 and 99.');
+		}
+
+		message.channel.bulkDelete(amount, true).catch(err => {
+			console.error(err);
+			message.channel.send('there was an error trying to prune messages in this channel!');
+		});
+   }
+
+});
